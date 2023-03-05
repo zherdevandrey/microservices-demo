5a456d0cb0cfc3dff49e9c3c2756502984014b80
andrey <mr.zherdev1992@gmail.com>
2023-03-05 19:54:01 +0100

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+jvm_memory_used_bytes{area="heap",id="G1 Eden Space",instance="elastic-query-service-1:8183",job="elastic-query-service"}
+http://localhost:9200/_cluster/health/?level=shards
---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/error/handler/ElasticQueryServiceErrorHandler.java
@@ -0,0 +1,47 @@
+package com.microservices.demo.elastic.query.service.api.error.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ElasticQueryServiceErrorHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ElasticQueryServiceErrorHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handle(IllegalArgumentException e) {
+        LOG.error("Illegal argument exception!", e);
+        return ResponseEntity.badRequest().body("Illegal argument exception! " + e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handle(RuntimeException e) {
+        LOG.error("Service runtime exception!", e);
+        return ResponseEntity.badRequest().body("Service runtime exception! " + e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception e) {
+        LOG.error("Internal server error!", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("A server error occurred!");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handle(MethodArgumentNotValidException e) {
+        LOG.error("Method argument validation exception!", e);
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(error ->
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
